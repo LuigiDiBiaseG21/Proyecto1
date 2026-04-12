d64fe0e91965f33a6ee172bceba79ae657fa8eb8
Juancabel <jc.beltrang1@uniandes.edu.co>
2026-04-11 20:37:52 -0500

    Another fix to 500 internal server errror

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,9 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_jwt_extended import JWTManager
+from flask_jwt_extended.exceptions import NoAuthorizationError, InvalidTokenError, ExpiredSignatureError, RevokedTokenError
 from flask_migrate import Migrate
 from .config import Config
 
@@ -21,22 +22,22 @@ def create_app(config_class=Config):
     ma.init_app(application)
     jwt.init_app(application)
 
-    # JWT Error Handlers - Ensure 401 responses for auth errors
-    @jwt.expired_token_loader
-    def expired_token_callback(jwt_header, jwt_payload):
-        return {"msg": "Token has expired"}, 401
+    # Flask Error Handlers for JWT Exceptions - Catch before Flask-RESTful
+    @application.errorhandler(NoAuthorizationError)
+    def handle_no_authorization(error):
+        return jsonify({"msg": "Missing Authorization Header"}), 401
 
-    @jwt.invalid_token_loader
-    def invalid_token_callback(error):
-        return {"msg": "Invalid token"}, 401
+    @application.errorhandler(InvalidTokenError)
+    def handle_invalid_token(error):
+        return jsonify({"msg": "Invalid token"}), 401
 
-    @jwt.unauthorized_loader
-    def missing_token_callback(error):
-        return {"msg": "Missing Authorization Header"}, 401
+    @application.errorhandler(ExpiredSignatureError)
+    def handle_expired_token(error):
+        return jsonify({"msg": "Token has expired"}), 401
 
-    @jwt.revoked_token_loader
-    def revoked_token_callback(jwt_header, jwt_payload):
-        return {"msg": "Token has been revoked"}, 401
+    @application.errorhandler(RevokedTokenError)
+    def handle_revoked_token(error):
+        return jsonify({"msg": "Token has been revoked"}), 401
     
     # 3. Importar modelos antes de inicializar Migrate
     # Esto permite que Alembic (el motor de Migrate) vea las tablas
